483ab1677cf120711a05067af7d91498f12010dd
Unknown <pifi77@hotmail.fr>
2018-04-09 15:05:01 +0200

    Underscore work
    The underscore_test works, wait for Hamza to finalize their work to change theirs.

--- src/main/webapp/underscore_test.js
+++ src/main/webapp/underscore_test.js
@@ -14,13 +14,29 @@
 this.tmpl = _.template($('#Test01').html());
 this.$element.append(this.tmpl(this.object));
 */
+var obj = {
+    name: 'Horace',
+    username: "Pifi",
+};
+var obj2 = {
+    name: 'Popo',
+    username: "damiLeBoss",
+};
+var obj3 = {
+    name: 'Jj',
+    username: "d4rkl1nk",
+};
+
 
 var testObject = _.template($('#test02').html());
 
-var html = testObject({
-    "attribute": JSON.stringify(result)
-});
+var html = testObject(obj);
+var html2 = testObject(obj2);
+var html3 = testObject(obj3);
 
 $('#result').append(html);
+$('#result').append(html2);
+$('#result').append(html3);
+
 
 
