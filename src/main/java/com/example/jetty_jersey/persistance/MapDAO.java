c4488199fe567410fa47c3726e813dcbc5f8299c
jemmy4s@yahoo.com <jemmy4s@yahoo.com>
2018-04-26 12:33:19 +0200

    ajout des table , delete map, correction bug get map

--- src/main/java/com/example/jetty_jersey/persistance/MapDAO.java
+++ src/main/java/com/example/jetty_jersey/persistance/MapDAO.java
@@ -13,8 +13,6 @@ interface MapDAO {
 
 	void addMap(MMap instance);
 	//void updateMap(String name,String mapName,String placeName,tag,visib,msg);
-	
-	// comment je peux donner l'id?? void editMap(int id, MMap instance);
 	void deletePlace(MMap instance, Location l);
 	void deleteMap(MMap instance);
 }
--- src/main/java/com/example/jetty_jersey/persistance/MapDB.java
+++ src/main/java/com/example/jetty_jersey/persistance/MapDB.java
@@ -1,5 +1,6 @@
 package com.example.jetty_jersey.persistance;
 
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.document.DocumentField;
@@ -7,10 +8,12 @@ import org.elasticsearch.common.document.DocumentField;
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 import org.elasticsearch.action.search.MultiSearchResponse;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.reindex.BulkByScrollResponse;
 import org.elasticsearch.index.reindex.UpdateByQueryAction;
@@ -38,11 +41,12 @@ public class MapDB implements MapDAO {
 
 	*
 	 */
-	public void addLocation(Location l) throws IOException{
+	public void addLocation(String idMap,Location l) throws IOException{
 		TransportClient client = Bdd.connectionToBD();
-		IndexResponse response = client.prepareIndex("map", "map")
+		IndexResponse response = client.prepareIndex("location","location")
 		        .setSource(jsonBuilder()
 		                    .startObject()
+		                    	.field("idMap",idMap)
 		                        .field("place", l.place)
 		                        .field("lat", ""+ l.lat)
 		                        .field("lng", ""+l.lng)
@@ -55,8 +59,29 @@ public class MapDB implements MapDAO {
 
 	}
 
-	public  MMap[] getMaps(User u) {
-		TransportClient client = Bdd.connectionToBD();
+public  MMap[] getMaps(User U) {
+
+	TransportClient client = Bdd.connectionToBD();
+	SearchResponse response = client.prepareSearch("map")
+    .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+    .setQuery(QueryBuilders.matchPhraseQuery("creator", U.login))
+    .get();
+	ArrayList<MMap> tab =new ArrayList<MMap>();		 
+	SearchHit[] hitTab = response.getHits().getHits();
+	for(int i = 0; i < hitTab.length ; i++) {
+		SearchHit hit = hitTab[i];
+		String location = (String) hit.getSourceAsMap().get("location");
+		String name = (String) hit.getSourceAsMap().get("name");
+		String visibilite = (String) hit.getSourceAsMap().get("visibilite");
+		tab.add(new MMap(name,U,visibilite));
+	}
+	MMap  [] tab2 = new MMap  [tab.size()]; 
+	for(int i = 0; i < tab.size(); i++){
+		tab2[i] = tab.get(i);
+	}
+	return tab2;
+}
+	/*TransportClient client = Bdd.connectionToBD();
 		System.out.println("get maps");
 		SearchRequestBuilder srb2 = client
 			    .prepareSearch().setQuery(QueryBuilders.matchQuery("creator", u.login )).setSize(1);
@@ -72,12 +97,14 @@ public class MapDB implements MapDAO {
 				/*Map<String, DocumentField> reponseFields = hits.getAt(i).getFields();
 				DocumentField d = reponseFields.get("place");
 				String place = d.getValue();
-				System.out.println(item.toString());*/
+				System.out.println(item.toString());
 
 			}
 		}
 		return null;
 	}
+*/
+	
 
 	/*
 	 * 	public String name;
@@ -94,7 +121,7 @@ public class MapDB implements MapDAO {
 			        .setSource(jsonBuilder()
 			                    .startObject()
 			                        .field("name", instance.name)
-			                        .field("user", instance.creator.login)
+			                        .field("creator", instance.creator.login)
 			                        .field("location", "id" )
 			                        .field("visibilite", instance.visibilite )
 			                    .endObject()
@@ -108,23 +135,83 @@ public class MapDB implements MapDAO {
 
 
 	public String[] getListMapName(User U) {
-		// TODO Auto-generated method stub
-		return null;
+		TransportClient client = Bdd.connectionToBD();
+		SearchResponse response = client.prepareSearch("map")
+        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+        .setQuery(QueryBuilders.matchPhraseQuery("creator", U.login))
+        .get();
+		ArrayList<String> tab =new ArrayList<String>();		 
+		SearchHit[] hitTab = response.getHits().getHits();
+		for(int i = 0; i < hitTab.length ; i++) {
+			SearchHit hit = hitTab[i];
+			String location = (String) hit.getSourceAsMap().get("location");
+			String name = (String) hit.getSourceAsMap().get("name");
+			String visibilité = (String) hit.getSourceAsMap().get("visibilite");
+			tab.add(name);
+		}
+		return (String[]) tab.toArray();
 	}
 
 	public MMap InfoLocation(String login, String mapName, String mapPlace) {
 		// TODO Auto-generated method stub
+		TransportClient client = Bdd.connectionToBD();
+		SearchResponse response = client.prepareSearch("map")
+        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+        .setQuery(QueryBuilders.matchPhraseQuery("name", mapName)) // rajouter le createur
+        .get();
+				 
+		SearchHit[] hitTab = response.getHits().getHits();
+		if(hitTab.length != 0) {
+			SearchHit hit = hitTab[0];
+			String location = (String) hit.getSourceAsMap().get("location");
+			String name = (String) hit.getSourceAsMap().get("name");
+			String visibilité = (String) hit.getSourceAsMap().get("visibilite");
+			return new MMap(mapName,new User(login,null),visibilité);
+			
+			//TODO SET LOCATION 
+		}
 		return null;
+		
 	}
 
 	public void deletePlace(MMap instance, Location l) {
-		// TODO Auto-generated method stub
-		
+		TransportClient client = Bdd.connectionToBD();
+		SearchResponse response = client.prepareSearch("location")
+        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+        
+        .setQuery(QueryBuilders.matchPhraseQuery("name", instance.name))
+        .get();
+		SearchHit[] hitTab = response.getHits().getHits();
+		if(hitTab.length != 0) {
+			SearchHit hit = hitTab[0];
+			String id = hit.getId();
+			System.out.println(id);
+			DeleteResponse response2 = client.prepareDelete("map", "map", id).get();		
+ 
+		}
+		else{
+			System.out.println("Aucune map trouve");
+		}
 	}
 
 	public void deleteMap(MMap instance) {
-		// TODO Auto-generated method stub
-		
+		TransportClient client = Bdd.connectionToBD();
+		SearchResponse response = client.prepareSearch("map")
+        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+        
+        .setQuery(QueryBuilders.matchPhraseQuery("name", instance.name))
+        .get();
+		SearchHit[] hitTab = response.getHits().getHits();
+		if(hitTab.length != 0) {
+			SearchHit hit = hitTab[0];
+			String id = hit.getId();
+			System.out.println(id);
+			DeleteResponse response2 = client.prepareDelete("map", "map", id).get();		
+ 
+		}
+		else{
+			System.out.println("Aucune map trouve");
+		}
 	}
 
 }
